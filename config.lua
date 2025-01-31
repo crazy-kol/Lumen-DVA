5b5f7b33fabaffe51ddd0f7603d37a61a6209c44
crazy-kol <uskolbee@gmail.com>
2025-01-30 19:58:36 -0800

    Update config.lua credit to: DaniBoi

--- config.lua
+++ config.lua
@@ -2,14 +2,9 @@
 ------Created By Whit3Xlightning & Lumen Studios------
 --https://github.com/crazy-kol https://github.com/Whit3Xlightning --
 --------------------------------------
-
-Config = {}
-
 Config = {
-
-    Webhook = 'WEBHOOK'
+  Webhook = 'WEBHOOK' -- Add you're webhook here!
     restricCommand = true
-    -- Setting this to false will allow anyone in the server to use the command. 
-    -- If you set it to true you will need to add a ace perm to allow people to use it.
-    -- Such as add_ace [GROUP] staff.cleanup allow
+
 }
+
